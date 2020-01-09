7bea233c9980094fafa074f19d82c8d7b09fb7a9
barrydevp <barrydevp@gmail.com>
2020-01-09 19:19:33 +0700

    fix 1.0.3 prepareInitialState

--- src/prepareInitialState.js
+++ src/prepareInitialState.js
@@ -1,11 +1,11 @@
-import { Log } from "./utils";
+import { Log, is } from "./utils";
 
 export default function(models = []) {
   const initialState = Object.values(models).reduce((previous, model) => {
     const iniState = model.state;
     const namespace = model.namespace;
 
-    if (!iniState || !namespace) {
+    if (is.undef(iniState) || !namespace) {
       Log.warn(`iniState || namespace is undefined`);
 
       return previous;
